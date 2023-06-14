c0a3ab03c6b6c46f768cb750500134fb4f192283
imran <imranance99@gmail.com>
2023-06-14 19:57:12 +0530

    My TodoList App

---
+++ function.py
@@ -0,0 +1,13 @@
+
+def get_todos(filepath = "todos.txt"):
+    with open(filepath, 'r') as file:
+        todos = file.readlines()
+    return todos
+
+def write_todos(todos_arg, filepath= "todos.txt"):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
